64cba558d2393f48d99bd7118948138a59d24ec4
Shunsuke Hochu <124444636+itukahashinu@users.noreply.github.com>
2024-12-20 20:07:31 +0900

    templates/postにsnsのデザイン例を追加

--- sns/post/apps.py
+++ sns/post/apps.py
@@ -4,3 +4,7 @@ from django.apps import AppConfig
 class PostConfig(AppConfig):
     default_auto_field = 'django.db.models.BigAutoField'
     name = 'post'
+    
+    def ready(self):
+        print("postアプリが起動しました!(post/apps.pyにて表示中)")
+    #Appconfigクラスにready()メソッドを用いるとアプリ起動時に実行される．
--- sns/post/urls.py
+++ sns/post/urls.py
@@ -5,6 +5,7 @@ app_name='post'
 
 main_urlpatterns=[
     path('',views.index,name='index')
+    #nameを用いるとurlが変更されても動的にnameの値で対応できる!!
 ]
 
 api_urlpatterns=[
