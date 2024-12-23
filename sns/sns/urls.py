9e3fda9dc4f05d074e33e3d0fff313d97c3711a6
Shunsuke Hochu <124444636+itukahashinu@users.noreply.github.com>
2024-12-23 23:04:01 +0900

    とりあえず保存

--- sns/sns/urls.py
+++ sns/sns/urls.py
@@ -25,7 +25,7 @@ urlpatterns = [
     path('',include('post.urls')),
     path('social/',include('social.urls')), #social関連のurlの先頭にはsocialをつける
     path('login',auth_views.LoginView.as_view(
-        tempate_name='legistration/login.html',
+        template_name='legistration/login.html',
         redirect_authenticated=True
     ),name='login'),
     path('logout',auth_views.LogoutView.as_view(next_page='/'),name='logput'),
