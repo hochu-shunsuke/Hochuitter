657dc95d8c55a5327bf990cb5e109a786cb5e912
Shunsuke Hochu <124444636+itukahashinu@users.noreply.github.com>
2024-12-25 22:10:54 +0900

    create_comment周りの実装開始．親postのデータをしっかりと渡しつつ関連付けたcommentを投稿するためのデータ受け渡しがうまく行っていない．

--- sns/post/urls.py
+++ sns/post/urls.py
@@ -1,9 +1,10 @@
 from django.urls import path
-from .views import create_post, index
+from .views import index,create_post,create_comment
 
 app_name='post'
 
 urlpatterns=[
     path('',index,name='index'),
     path('create/',create_post,name='create_post'), #投稿作成用のURL
+    path('create/comment/<int:post_id>',create_comment,name='create_comment')
 ]
--- sns/post/views.py
+++ sns/post/views.py
@@ -1,8 +1,7 @@
 from django.shortcuts import render,redirect
-from .models import Post
+from .models import Post,Comment
 from django.urls import reverse_lazy #処理成功後の遷移先urlを引数に持つ,『urlの遅延評価』というらしい．
-from .form import PostForm
-from django.contrib.auth.models import User
+from .form import PostForm,CommentForm
 from django.contrib.auth.decorators import login_required #ログイン状態でない場合はログインページにリダイレクトされる
 
 
@@ -17,7 +16,7 @@ def index(request):
     #親ポストを全て表示．
     user=request.user #Django標準の認証システムによりログインユーザを取得
     user_id=request.user.id
-    parent_posts=Post.objects.all()[:20] #最大数は開発段階ではとりあえず20
+    parent_posts=Post.objects.all()[:50] #最大数は開発段階ではとりあえず50
     return render(request,'post/index.html',{
         'parent_posts':parent_posts,#key:parent_postに対してvalue:parent_postを持つ辞書を返し，index.htmlにkeyを渡す．
         'username':user.username,
@@ -40,4 +39,22 @@ def create_post(request):
                   {'form':form,
                    'user':user,
                    'user_id':user.id} #実際にはcreate.htmlでextendしているbase.htmlの中で使用している
-                  ) #フォームをテンプレートに渡す
+    ) #フォームをテンプレートに渡す
+
+@login_required
+def create_comment(request,post_id): #post_idを必ず受け取ることを忘れずに
+    user=request.user
+    if request.method=='POST': #HTTPリクエストの種類を確認し適切に処理
+        form=CommentForm(request.Comment)
+        if form.is_valid():
+            comment=form.save(commit=False)
+            comment.user=request.user #現在のユーザ
+            comment.save()
+            return redirect('post:index') #投稿後にリダイレクト
+    else:
+        form=CommentForm() #GETリクエストの場合は空のフォームを作成
+    return render(request,'post/create_comment.html',
+                    {'form':form,
+                    'user':user,
+                    } #o親ポストを表示した上でコメントのフォームをcreate_html（作成済み)に設ける．
+    ) #フォームをテンプレートに返す
