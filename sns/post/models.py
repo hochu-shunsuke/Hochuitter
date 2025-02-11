bb13b979324f7acd0589242759e0042a1eb31050
Shunsuke Hochu <124444636+itukahashinu@users.noreply.github.com>
2025-02-11 12:53:55 +0900

    プロフィールとindexの完全な分離をした。

--- sns/post/models.py
+++ sns/post/models.py
@@ -2,63 +2,71 @@ from django.db import models
 from django.contrib.auth.models import User
 
 class Post(models.Model):
-    #models.Modelは常に存在するDjangoの基本クラスで、条件は常にFalse
-    content=models.CharField(max_length=280) #投稿内容
-    like_count=models.PositiveIntegerField(default=0) #いいね数
-    comment_count=models.PositiveIntegerField(default=0) #返信数(階層が1つ下の返信のみをカウント)
-    post_date=models.DateTimeField(auto_now_add=True) #投稿日時
-    user=models.ForeignKey(
-        User, #一人のユーザが複数のpostを持つため，ForeignKeyを使用． #TODO:マイページにてpost.user.all()でユーザの投稿一覧を作成．
+    content = models.CharField(max_length=280)  # 投稿内容
+    like_count = models.PositiveIntegerField(default=0)  # いいね数
+    comment_count = models.PositiveIntegerField(default=0)  # 返信数(階層が1つ下の返信のみをカウント)
+    post_date = models.DateTimeField(auto_now_add=True)  # 投稿日時
+    user = models.ForeignKey(
+        User,  # 一人のユーザが複数のpostを持つため，ForeignKeyを使用
         related_name='posts',
         on_delete=models.CASCADE,
     )
-    liked_users=models.ManyToManyField(User,related_name='liked_post',blank=True) #いいねしたユーザ
-    #null has no effect on ManyToManyField(ManyToManyFieldにはnullは影響しない)
+    liked_users = models.ManyToManyField(
+        User,
+        related_name='liked_post',
+        blank=True
+    )  # いいねしたユーザ
+    bookmarked_users = models.ManyToManyField(
+        User,
+        related_name='bookmarked_posts',
+        blank=True
+    )  # ブックマークしたユーザ
     
     def __str__(self):
         return self.content
             
     class Meta:
-        ordering=['-post_date'] #postを投稿順に並び替え
+        ordering = ['-post_date']  # postを投稿順に並び替え
         indexes = [
             models.Index(fields=['like_count']),
             models.Index(fields=['post_date']),
         ]
 
 class Comment(models.Model):
-    content=models.CharField(max_length=280)
-    like_count=models.PositiveIntegerField(default=0)
-    liked_users=models.ManyToManyField(User,related_name='liked_comments',blank=True) #いいねしたユーザ
-    comment_count=models.PositiveIntegerField(default=0) #返信数(階層が1つ下の返信のみをカウント)
-    post_date=models.DateTimeField(auto_now_add=True)
-    user=models.ForeignKey(
+    content = models.CharField(max_length=280)
+    like_count = models.PositiveIntegerField(default=0)
+    liked_users = models.ManyToManyField(
+        User,
+        related_name='liked_comments',
+        blank=True
+    )  # いいねしたユーザ
+    comment_count = models.PositiveIntegerField(default=0)  # 返信数(階層が1つ下の返信のみをカウント)
+    post_date = models.DateTimeField(auto_now_add=True)
+    user = models.ForeignKey(
         User,
         related_name='comments',
         on_delete=models.CASCADE
     )
-    post=models.ForeignKey(
-        #models.ForeignKeyの第一引数は必ずモデル名
+    post = models.ForeignKey(
         Post,
         related_name='comments',
         on_delete=models.CASCADE,
-        null=True,blank=True
+        null=True,
+        blank=True
     )
-    #null:databaseレベルの空,blank:フォームや管理画面レベルの空
-    #コメントに対するコメントの場合親Postが存在しないためこの記述が必要
-    parent_comment=models.ForeignKey(
-        #models.ForeignKeyの第一引数は必ずモデル名．今回は自身なので'self'
+    parent_comment = models.ForeignKey(
         'self',
-        related_name='replies', #リレーション名．使用時はreplies.parent_commment(repliesのparent)という形であるため
-        on_delete=models.CASCADE, #親投稿が削除されたら自動で削除される
-        null=True,blank=True #null:databaseレベルの空,blank:フォームや管理画面レベルの空
+        related_name='replies',
+        on_delete=models.CASCADE,
+        null=True,
+        blank=True
     )
     
     def __str__(self):
         return self.content if self.content else "No content"
     
     class Meta:
-        ordering=['-like_count','-post_date'] # 修正：投稿日時が新しい順に
-        #いいねが多い順->投稿日時が新しい順に変更
+        ordering = ['-like_count', '-post_date']
         indexes = [
             models.Index(fields=['like_count']),
             models.Index(fields=['post_date']),
